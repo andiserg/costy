4bfacbef2fad6e8238e928274a0243605bb7bc39
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-02-05 20:59:38 +0200

    edited script for testing Github Actions

--- env.py
+++ env.py
@@ -4,3 +4,7 @@ env_file = os.getenv("GITHUB_ENV", False)
 if env_file:
     with open(env_file, "r") as f:
         print(f.read())
+print(os.getenv("DB_USER"))
+print(os.getenv("DB_PASSWORD"))
+print(os.getenv("TEST_DB_NAME"))
+print(os.getenv("DB_HOST"))
