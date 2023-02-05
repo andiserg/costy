d07385b0ac38c89b6dffd58f1dd88105cae38c5e
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-02-05 19:32:10 +0200

    added tests

--- app/tests/main_test.py
+++ app/tests/main_test.py
@@ -1,5 +1,5 @@
 """
-Testing main.py
+Testing app.main
 """
 
 import pytest
@@ -20,3 +20,14 @@ async def test_root():
         response = await client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+@pytest.mark.anyio
+async def test_db_version():
+    """
+    Тестування app.main.say_db_version
+    """
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as client:
+        response = await client.get("/database_info/")
+        assert response.status_code == 200
+        assert "message" in response.json()
