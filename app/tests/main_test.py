c6f484627d6dbfc7ee86d47022f6b166d1e3ed9b
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-02-05 21:14:06 +0200

    application dependency is overridden by the test database in tests

--- app/tests/main_test.py
+++ app/tests/main_test.py
@@ -5,7 +5,9 @@ Testing app.main
 import pytest
 from httpx import AsyncClient
 
+from app.database.database import Database
 from app.main import app
+from app.main import database as default_database
 
 # Імпорт потрібний для роботи @pytest.mark.anyio
 from app.tests.config import anyio_backend, precents_evn_variables
@@ -28,6 +30,10 @@ async def test_db_version():
     """
     Тестування app.main.say_db_version
     """
+    database = Database(test=True)
+    app.dependency_overrides[
+        default_database.get_session_depends
+    ] = database.get_session_depends
     async with AsyncClient(app=app, base_url="http://127.0.0.1:8000") as client:
         response = await client.get("/database_info/")
         assert response.status_code == 200
