0b32e7d1b2d36609360e2586d41086e71544bc28
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-02-07 23:35:46 +0200

    added read_user endpoint and his test

--- app/tests/func/users_test.py
+++ app/tests/func/users_test.py
@@ -21,5 +21,23 @@ async def test_create_user_endpoint(client_db: AsyncClient):  # noqa: F811;
     """
     data = {"email": "test@mail.test", "password": "123456"}  # nosec B106
     result = await client_db.post("/users/create/", json=data)
-    assert result.status_code == 200
+    assert result.status_code == 201
     assert all(key in result.json() for key in ["id", "email"])
+
+
+@pytest.mark.asyncio
+async def test_read_user(client_db: AsyncClient):  # noqa: F811;
+    """
+    Testing app.views.users.read_me
+    """
+    data = {"email": "test", "username": "test", "password": "test"}  # nosec B106
+    await client_db.post("/users/create/", json=data)
+
+    auth_result = await client_db.post("/token/", data=data)
+    token = f"Bearer {auth_result.json()['access_token']}"
+
+    read_result = await client_db.get("/users/me/", headers={"Authorization": token})
+    assert read_result.status_code == 200
+
+    user_data = read_result.json()
+    assert user_data["email"] == data["email"]
--- app/views/users.py
+++ app/views/users.py
@@ -4,14 +4,16 @@ Users endpoints
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.crud.auth import get_current_user_depend
 from app.crud.users import create_user
 from app.main import get_session
+from app.models.users import User
 from app.schemas.users import UserCreateSchema, UserSchema
 
 router = APIRouter(prefix="/users")
 
 
-@router.post("/create/", response_model=UserSchema)
+@router.post("/create/", response_model=UserSchema, status_code=201)
 async def create_user_view(
     user: UserCreateSchema,
     session: AsyncSession = Depends(get_session),
@@ -21,3 +23,11 @@ async def create_user_view(
     """
     created_user = await create_user(session, user)
     return created_user
+
+
+@router.get("/me/", response_model=UserSchema)
+async def read_me(current_user: User = Depends(get_current_user_depend)):
+    """
+    Повертає залогіненого юзера, або 401, якщо юзер не вказав заголовок Authorization
+    """
+    return current_user
