3efc06d9b743c1d7bba0ce09f82feaa6fd01fcc0
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-02-09 19:41:22 +0200

    refactored parts of the code that are placed in methods

--- app/main.py
+++ app/main.py
@@ -10,11 +10,13 @@ get_session = database.get_session_depends
 
 app = FastAPI()
 
-from app.views.auth import router as auth_router  # noqa: E402;
 
 # Імопрти роутерів нижче ніж створення бази і додатку тому що вони їх імпортують у себе.
 # Тому якщо імпорти поставити вище, то буде помилка Cyclic Import Error
+from app.views.auth import router as auth_router  # noqa: E402;
+from app.views.operations import router as operations_router  # noqa: E402;
 from app.views.users import router as users_router  # noqa: E402;
 
 app.include_router(users_router, tags=["users"])
 app.include_router(auth_router, tags=["auth"])
+app.include_router(operations_router, tags=["operations"])
