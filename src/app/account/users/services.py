b269d9d094c873e86b15814270e6c6d45a985a17
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-04 10:28:59 +0300

    migrated services to work with the unit of work pattern

--- src/app/account/users/services.py
+++ src/app/account/users/services.py
@@ -1,21 +1,21 @@
 """
 CRUD операції з User сутностями
 """
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from src.app.account.auth.password import get_password_hash
 from src.app.account.users.models import User
+from src.app.unit_of_work import AbstractUnitOfWork
 from src.schemas.users import UserCreateSchema
 
 
-async def create_user(session: AsyncSession, user: UserCreateSchema) -> User:
+async def create_user(uow: AbstractUnitOfWork, user: UserCreateSchema) -> User:
     """
-    :param session: Сесія ДБ. Див. src.database.database.Database
+    :param uow: Unit of Work
     :param user: Схема для створення користувача
     :return: src.models.users.User
     """
     hashed_password = get_password_hash(user.password)
-    db_user = User(email=user.email, hashed_password=hashed_password)
-    session.add(db_user)
-    await session.commit()
-    return db_user
+    async with uow:
+        db_user = User(email=user.email, hashed_password=hashed_password)
+        await uow.users.add(db_user)
+        await uow.commit()
+        return db_user
