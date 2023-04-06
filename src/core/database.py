f3119d5f7aad23f60b9fb4af8fa37d5bd3863c1a
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-06 09:34:16 +0300

    created DatabaseFactory singleton

--- src/core/database.py
+++ src/core/database.py
@@ -3,10 +3,11 @@
 """
 import os
 
+from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import registry
 
-mapper_registry = registry()
+from src.app.adapters.orm import create_tables, start_mappers
 
 
 def _get_url(test: bool = False) -> str:
@@ -37,9 +38,10 @@ class Database:
         Використовується під час pytest тестів.
     """
 
-    def __init__(self, test: bool = False):
+    def __init__(self, mapper_registry, test: bool = False):
         self.engine = create_async_engine(_get_url(test), echo=True)
         self.sessionmaker = async_sessionmaker(self.engine, expire_on_commit=False)
+        self.mapper_registry = mapper_registry
 
         self.test = test
         # db_created використовується як прапорець того, створена база чи ні
@@ -55,29 +57,52 @@ class Database:
         тому ці методи виконуються у синхронному режимі за допомогою run_sync
         """
         async with self.engine.begin() as conn:
-            await conn.run_sync(mapper_registry.metadata.drop_all)
-            await conn.run_sync(mapper_registry.metadata.create_all)
+            await conn.run_sync(self.mapper_registry.metadata.drop_all)
+            await conn.run_sync(self.mapper_registry.metadata.create_all)
 
-    async def get_session_depends(self) -> AsyncSession:
+    async def get_session(self) -> AsyncSession:
         """
         Дає AsyncSession для роботи з базою
         Увага:
             Призначена для FastApi Depends, тому що сесія повертається через yield
             В якості Depends метод відпрацює правильно,
             але виклили з інших мість можуть створити неочікувані наслідки.
-            Використовуйте наступний шаблон для правильного отримання сесії
-
-                database = Database()
-                async with database.sessionmaker() as session:
-                    <code>
-
         :return: sqlalchemy.ext.asyncio.AsyncSession
         """
         if self.test and not self.db_created:
-            # Тут це для того, щоб виконатись у FastApi event loop
-            # Якщо виконати asyncio.run(init_models()),
-            # то далі робота з ORM буде неможлива
+            # Тут це для того, щоб виконатись у async event loop
             await self.init_models()
             self.db_created = True
         async with self.sessionmaker() as session:
             yield session
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class DatabaseFactory(metaclass=SingletonMeta):
+    def __init__(self):
+        self.mapper_registry = registry()
+        self._bootstrap_tables()
+
+    def _bootstrap_tables(self):
+        tables = create_tables(self.mapper_registry)
+        start_mappers(self.mapper_registry, tables)
+
+    def get_database(self, test=False) -> Database:
+        return Database(self.mapper_registry, test=test)
+
+
+def bind_database_to_app(app: FastAPI, database: Database):
+    app.dependency_overrides[get_session_depend] = database.get_session
+
+
+async def get_session_depend():
+    """Функція-заглушка"""
+    pass
