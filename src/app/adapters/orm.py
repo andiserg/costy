ce4d138996947bfb3c3dd4ffe78341b330eb7d09
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-06 09:37:29 +0300

    table creation code extracted into a method

--- src/app/adapters/orm.py
+++ src/app/adapters/orm.py
@@ -6,40 +6,41 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Table
 
 from src.app.account.users.models import User
 from src.app.operations.models import Operation
-from src.core.database import mapper_registry
-
-users = Table(
-    "users",
-    mapper_registry.metadata,
-    Column("id", Integer, primary_key=True),
-    Column("email", String, unique=True, index=True, nullable=False),
-    Column("hashed_password", String, nullable=False),
-)
 
 
-# Таблиця операцій (транзакції, витрати).
-operations = Table(
-    "operations",
-    mapper_registry.metadata,
-    Column("id", Integer, primary_key=True),
-    Column("amount", Integer, nullable=False),
-    Column("description", String),
-    Column("unix_time", Integer, nullable=False),
-    Column("mcc", Integer),
-    # mcc - код виду операції
-    Column("source_type", String, nullable=False),
-    # Тип джерела.
-    # value: "manual" | "<bank_name>"
-    # Операція може бути або додана вручну або за допомогою API банку
-    Column("user_id", Integer, ForeignKey("users.id")),
-)
+def create_tables(mapper_registry) -> dict[str, Table]:
+    return {
+        "users": Table(
+            "users",
+            mapper_registry.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("email", String, unique=True, index=True, nullable=False),
+            Column("hashed_password", String, nullable=False),
+        ),
+        "operations": Table(
+            "operations",
+            mapper_registry.metadata,
+            Column("id", Integer, primary_key=True),
+            Column("amount", Integer, nullable=False),
+            Column("description", String),
+            Column("unix_time", Integer, nullable=False),
+            Column("mcc", Integer),
+            # mcc - код виду операції
+            Column("source_type", String, nullable=False),
+            # Тип джерела.
+            # value: "manual" | "<bank_name>"
+            # Операція може бути або додана вручну або за допомогою API банку
+            Column("user_id", Integer, ForeignKey("users.id")),
+        ),
+    }
 
 
-def start_mappers():
+def start_mappers(mapper_registry, tables):
     """
     Прив'язка класів до таблиць. ORM буде з ними працювати як з моделями
     Такий підхід відповідє SOLID, інверсуючи залежності.
     Таким чином, частини програми нижчого рівня стають залежними від вищого рівня
     """
-    mapper_registry.map_imperatively(User, users)
-    mapper_registry.map_imperatively(Operation, operations)
+    mapper_registry
+    mapper_registry.map_imperatively(User, tables["users"])
+    mapper_registry.map_imperatively(Operation, tables["operations"])
