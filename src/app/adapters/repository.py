57b187bda5cdaeed38f9715398f6e41d8b8faedd
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-12 13:45:34 +0300

    extracted repositories from repositories.py to app/repositories

--- src/app/adapters/repository.py
+++ src/app/adapters/repository.py
@@ -9,10 +9,6 @@ from abc import ABC, abstractmethod
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.app.account.users.models import User
-from src.app.bank_managers.models import Manager, ManagerProperty
-from src.app.operations.models import Operation
-
 
 class AbstractRepository(ABC):
     @abstractmethod
@@ -34,46 +30,3 @@ class SqlAlchemyRepository(AbstractRepository, ABC):
 
     async def _get(self, model, field, value):
         return await self.session.scalar(select(model).filter_by(**{field: value}))
-
-
-class UserRepository(SqlAlchemyRepository):
-    async def get(self, field, value) -> User:
-        return await self._get(User, field, value)
-
-
-class OperationRepository(SqlAlchemyRepository):
-    async def get(self, field, value) -> Operation:
-        return await self._get(Operation, field, value)
-
-    async def get_all_by_user(self, user_id) -> list[Operation]:
-        return list(
-            await self.session.scalars(select(Operation).filter_by(user_id=user_id))
-        )
-
-
-class ManagerRepository(SqlAlchemyRepository):
-    async def get(self, field, value) -> Manager:
-        return await self._get(Manager, field, value)
-
-    async def get_properties(
-        self, manager: Manager
-    ) -> list[dict[str, str | int | float]]:
-        return [
-            property.as_dict
-            for property in list(
-                await self.session.scalars(
-                    select(ManagerProperty).filter_by(manager_id=manager.id)
-                )
-            )
-        ]
-
-    async def add_property(self, manager: Manager, name: str, value: str | int | float):
-        types = {str: "str", int: "int", float: "float"}
-        self.session.add(
-            ManagerProperty(
-                name=name,
-                value=str(value),
-                type=types[type(value)],
-                manager_id=manager.id,
-            )
-        )
---
+++ src/app/bank_managers/monobank/repositories.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+import aiohttp
+
+from src.app.bank_managers.services import BankManagerRepository
+from src.app.operations.models import Operation
+
+
+class MonobankManagerRepository(BankManagerRepository):
+    __bankname__ = "monobank"
+    MAX_UPDATE_PERIOD = timedelta(30)
+
+    async def get_costs(
+        self, from_time=datetime.now(), to_time=datetime.now() - MAX_UPDATE_PERIOD
+    ) -> list[Operation]:
+        url = "https://api.monobank.ua/personal/statement/0"
+        headers = {"X-Token": self.properties["X-Token"]}
+        result_operations = []
+        async with aiohttp.ClientSession(headers=headers) as session:
+            while from_time:
+                response = await session.get(f"{url}/{from_time}/{to_time}")
+                operations = await response.json()
+                costs = list(filter(lambda operation: operation.amount < 0, operations))
+                result_operations.append(*costs)
+                from_time = operations[-1]["time"] if len(operations) == 500 else None
+                # В одного запиту до Monobank API обмеження - 500 операцій
+                # Тому, якщо вийшло 500 операцій то можливо, ще є операції
+                # В такому випадку, потрібно зробити ще один запит
+                # Від часу остальної операцій до to_time
+        return [Operation(**operation) for operation in result_operations]
