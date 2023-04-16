67353c90c6e5d57e0d62c0cf51d1270ce30367e6
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-16 13:54:33 +0300

    refactored imports

--- src/app/repositories/users.py
+++ src/app/repositories/users.py
@@ -1,7 +1,8 @@
 from src.app.domain.users import User
+from src.app.repositories.absctract.users import AUserRepository
 from src.app.repositories.sqlalchemy import SqlAlchemyRepository
 
 
-class UserRepository(SqlAlchemyRepository):
+class UserRepository(SqlAlchemyRepository, AUserRepository):
     async def get(self, field, value) -> User:
         return await self._get(User, field, value)
