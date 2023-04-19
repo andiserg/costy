ff6a7e3d5e37036915e1043915a358814e20fcf1
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-04-19 11:53:22 +0300

    created statistic router

--- tests/func/statistic_test.py
+++ tests/func/statistic_test.py
@@ -1,25 +1,16 @@
 import random
-from datetime import datetime
 
 import pytest
+from httpx import AsyncClient
 
-from src.app.domain.operations import Operation
-from src.app.services.statistic import get_statistic
-from src.database import Database
+from tests.patterns import create_and_auth_func_user, create_operations
 
 
 @pytest.mark.asyncio
-async def test_get_statistic(database: Database):
-    operations = [
-        Operation(
-            amount=random.randint(-10000, -10),
-            description="description",
-            mcc=random.randint(9996, 9999),
-            source_type="manual",
-            time=int(datetime.now().timestamp()),
-            user_id=1,
-        )
-        for _ in range(10)
-    ]
-    statistic = get_statistic(operations)
-    print(1)
+async def test_get_statistic(client_db: AsyncClient):
+    auth_data = await create_and_auth_func_user(client_db)
+    token = auth_data["token"]
+    headers = {"Authorization": token}
+    await create_operations(headers, client_db)
+    response = await client_db.get("/statistic/", headers=headers)
+    assert response.status_code == 200
