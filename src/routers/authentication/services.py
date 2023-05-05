bd9814e47b7ac00b1583a56ac7097b3c31fff556
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-05-05 14:42:56 +0300

    refactored authenticate user algorithm

--- src/routers/authentication/services.py
+++ src/routers/authentication/services.py
@@ -5,35 +5,12 @@ from datetime import datetime, timedelta
 
 from jose import JWTError, jwt
 
-from src.app.domain.users import User
-from src.app.services.uow.abstract import AbstractUnitOfWork
 from src.routers.authentication.config import (
     ACCESS_TOKEN_EXPIRE_MINUTES,
     ALGORITHM,
     SECRET_KEY,
 )
-from src.routers.authentication.password import verify_password
 from src.schemas.auth import TokenData
-
-
-async def authenticate_user(
-    uow: AbstractUnitOfWork, email: str, password: str
-) -> User | None:
-    """
-    Аутентифікація користувача.
-    По даним для входу шукається, перевіряється на співпадіння паролів
-    та вертається User модель
-
-    :param uow: Unit of Work
-    :param email: email
-    :param password: password
-    :return: User якщо аутентифікація вдала, None якщо ні
-    """
-    async with uow:
-        user = await uow.users.get("email", email)
-        if not user or not verify_password(password, user.hashed_password):
-            return
-        return user
 
 
 def create_access_token(data: dict) -> str:
