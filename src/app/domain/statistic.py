6968c06f10b7ae2228b47afa3859281020f41008
Andrew Sergienko <sergienkoandrew9922@gmail.com>
2023-05-25 22:33:01 +0300

    added new field to statistic

--- src/app/domain/statistic.py
+++ src/app/domain/statistic.py
@@ -6,3 +6,4 @@ class Statistic:
     costs_sum: int
     categories_costs: dict[int, int]
     costs_num_by_days: dict[str, int]
+    costs_sum_by_days: dict[str, int]
--- src/schemas/statistic.py
+++ src/schemas/statistic.py
@@ -7,3 +7,4 @@ class StatisticSchema(BaseModel):
     costs_sum: int
     categories_costs: dict[int | None, int]
     costs_num_by_days: dict[str, int]
+    costs_sum_by_days: dict[str, int]
